cce9c8e09e1ed2bee462eaae586acfd4542ebf1e
sunjuyoung <syseoz@naver.com>
2022-04-07 23:47:33 +0900

    strategy pattern

--- advanced/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ advanced/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -1,7 +1,9 @@
 package hello.advanced.trace.strategy;
 
 import hello.advanced.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.trace.strategy.code.strategy.Strategy;
 import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
 import hello.advanced.trace.template.code.AbstractTemplate;
 import hello.advanced.trace.template.code.SubClassLogic1;
 import hello.advanced.trace.template.code.SubClassLogic2;
@@ -11,30 +13,40 @@ import org.junit.jupiter.api.Test;
 @Slf4j
 public class ContextV1Test {
 
+    //선 조립 후 실행 방법
     @Test
     public void strategyV2() throws Exception{
-       //StrategyLogic에 메서드가 한개만 있어야한다
-       ContextV1 contextV1 = new ContextV1(()->log.info("비즈니스 로직1"));
-       contextV1.execute();
-       //when
+        Strategy strategy1 = new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직 1");
+            }
+        };
+        ContextV1 contextV1 = new ContextV1(strategy1);
+        contextV1.execute();
 
-       //then
+        ContextV1 contextV2 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 2");
+            }
+        });
+
+        //인터페이스 메서드가 1개만있으면 익명 내부클래스를 람다로 
+        ContextV1 contextV3 = new ContextV1(()-> log.info("비즈니스 3"));
+        contextV3.execute();
 
     }
 
     @Test
     public void strategyV1() throws Exception{
-       //given
         StrategyLogic1 strategyLogic1 = new StrategyLogic1();
         ContextV1 contextV1 = new ContextV1(strategyLogic1);
         contextV1.execute();
-        //when
 
-        StrategyLogic1 strategyLogic2 = new StrategyLogic1();
-        ContextV1 contextV2 = new ContextV1(strategyLogic1);
+        StrategyLogic2 strategyLogic2 = new StrategyLogic2();
+        ContextV1 contextV2 = new ContextV1(strategyLogic2);
         contextV2.execute();
-       //then
-       
     }
 
     @Test
--- advanced/src/test/java/hello/advanced/trace/strategy/ContextV2Test.java
+++ advanced/src/test/java/hello/advanced/trace/strategy/ContextV2Test.java
@@ -1,9 +1,6 @@
 package hello.advanced.trace.strategy;
 
-import hello.advanced.trace.strategy.code.strategy.ContextV1;
-import hello.advanced.trace.strategy.code.strategy.ContextV2;
-import hello.advanced.trace.strategy.code.strategy.Strategy;
-import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
+import hello.advanced.trace.strategy.code.strategy.*;
 import hello.advanced.trace.template.code.AbstractTemplate;
 import hello.advanced.trace.template.code.SubClassLogic1;
 import hello.advanced.trace.template.code.SubClassLogic2;
@@ -12,6 +9,7 @@ import org.junit.jupiter.api.Test;
 
 @Slf4j
 public class ContextV2Test {
+
 
 
     @Test
@@ -35,25 +33,16 @@ public class ContextV2Test {
        //StrategyLogic에 메서드가 한개만 있어야한다
        ContextV1 contextV1 = new ContextV1(()->log.info("비즈니스 로직1"));
        contextV1.execute();
-       //when
 
-       //then
 
     }
 
     @Test
     public void strategyV1() throws Exception{
-       //given
-        StrategyLogic1 strategyLogic1 = new StrategyLogic1();
-        ContextV1 contextV1 = new ContextV1(strategyLogic1);
-        contextV1.execute();
-        //when
+        ContextV2 contextV2 = new ContextV2();
 
-        StrategyLogic1 strategyLogic2 = new StrategyLogic1();
-        ContextV1 contextV2 = new ContextV1(strategyLogic1);
-        contextV2.execute();
-       //then
-       
+        contextV2.execute(new StrategyLogic1());
+        contextV2.execute(new StrategyLogic2());
     }
 
 
